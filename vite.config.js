ec4ad85f10b4e57dd775dc7aa90e5616114ec146
twl640 <tsangwailok@gmail.com>
2026-02-11 16:47:52 +0800

    Set base path for GitHub Pages subfolder

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/pwa_ocr/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
